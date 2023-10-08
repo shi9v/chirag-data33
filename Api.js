78007d03999d128bb2308cc05ba4d669d8578139
shi9v <136371901+shi9v@users.noreply.github.com>
2023-10-08 19:35:33 +0530

    Initial commit

---
+++ Api.js
@@ -0,0 +1,96 @@
+const express=require("express")
+const app=express()
+const bodyP=require("body-parser")
+const compiler=require("compilex")
+const options={stats:true}
+compiler.init(options)
+app.use(bodyP.json())
+app.use("/codemirror-5.65.14",express.static("E:/Compiler/codemirror-5.65.14"))
+app.use("/",function(req,res){
+    res.sendFile("E:/Compiler/index.html")
+}) 
+app.post("/compile",function(req,res){
+    var code=req.body.code
+    var input=req.body.input
+    var lang=req.body.lang
+    var envData = {OS : "windows"}; 
+   try{
+    if(lang=="C++"){
+        if(!input){
+            var envData = { OS : "windows" , cmd : "g++",options:{timeout:10000}}; // (uses g++ command to compile )
+            compiler.compileCPP(envData , code , function (data) {
+                if(data.output){
+                  res.send(data);
+                }
+                else{
+                   res.send({output:"error"})
+                }
+            });
+        }
+        else{
+            var envData = { OS : "windows" , cmd : "g++",options:{timeout:10000}}; // (uses g++ command to compile )
+            compiler.compileCPPWithInput(envData , code , input , function (data) {
+                if(data.output){
+                    res.send(data);
+                }
+                else{
+                    res.send({output:"error"})
+                }
+            });
+        }
+    }
+    else if(lang=="Java"){
+        if(!input){
+            var envData = { OS : "windows"}; 
+            compiler.compileJava( envData , code , function(data){
+                if(data.output){
+                    res.send(data);
+                }
+                else{
+                   res.send({output:"error"})
+                }
+            });    
+        } 
+        else if(lang="Python"){
+            var envData = { OS : "windows"}; 
+            compiler.compileJavaWithInput( envData , code , input ,  function(data){
+                if(data.output){
+        res.send(data);
+    }
+    else{
+        res.send({output:"error"})
+    }
+            });
+        }
+    }
+    else {
+        if(!input){
+            var envData = { OS : "windows"}; 
+            compiler.compilePython( envData , code , function(data){
+                if(data.output){
+        res.send(data);
+    }
+    else{
+        res.send({output:"error"})
+    }
+            });   
+        }
+        else{
+            var envData = { OS : "windows"}; 
+            compiler.compilePythonWithInput( envData , code , input ,  function(data){
+                if(data.output){
+                   res.send(data);
+                }
+                else{
+                   res.send({output:"error"})
+                }        
+            });
+        }
+    }
+
+   }catch(e){
+    console.log("error")
+   }  
+
+})
+app.listen(4000)
---
+++ README.md
@@ -0,0 +1,2 @@
+# chirag-data4
+ 
